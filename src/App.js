033b9d2e802fc9f74f0bccd8b7779f74c6d78947
MarciaOng <mrmarciaong@gmail.com>
2023-08-23 19:03:29 +0800

    initt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GCS JSON Viewer</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter GCS URI"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button onClick={fetchData}>Fetch JSON</button>
+        </div>
+        {error && <div className="error">{error}</div>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
       </header>
     </div>
   );
